73949c291890660c7c157ab79c8fdfd953ed6dd7
MatviienkoNataliia <admin@MatviienkoNataliias-Mac-mini.local>
2021-07-02 18:17:18 +0300

    mobile nata

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,10 +1,24 @@
 var swiper = new Swiper(".section-reviews__swiper", {
     loop:true,
     autoplay: {
-        delay: 1000,
+        delay: 3000,
       },
     navigation: {
       nextEl: ".section-reviews__button-next",
-      prevEl: "section-reviews__button-prev",
+      prevEl: ".section-reviews__button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 100
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 115
+      },
+    }
   });
