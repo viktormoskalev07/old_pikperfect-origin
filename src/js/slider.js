38e20f235bb9cc26b0b8e0df61920e311b3590cf
unknown <viktormoskalev07@gmail.com>
2021-07-12 10:24:24 +0300

    1

--- src/js/slider.js
+++ src/js/slider.js
@@ -52,12 +52,15 @@ var swiperTabs = new Swiper(".section-hero__swiper", {
 let checkVideo =1 ;
 const videoSlide = document.querySelector('.hero-video-slide');
   swiperTabs.on('slideChange', function(){
-    if (checkVideo){
+    console.log(videoSlide.matches('.swiper-slide-active'))
+      setTimeout(() => {
+        console.log(videoSlide.matches('.swiper-slide-active'))
+      }, 100);
+    if (checkVideo&&videoSlide){
 
         if(videoSlide.matches('.swiper-slide-next')||videoSlide.matches('.swiper-slide-active')){
           console.log(videoSlide.matches('.swiper-slide-next')||videoSlide.matches('.swiper-slide-active')) 
-                      video=document.getElementById('hero-video');
-                      console.log(video.dataset.src);
+                      video=document.getElementById('hero-video'); 
                       video.src=video.dataset.src;
                       videoScript=  document.getElementById('hero-video-script'); 
                       videoScript.src='https://player.vimeo.com/api/player.js'; 
