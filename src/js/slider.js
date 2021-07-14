8a9ab582ce75f1cdf5878499b9917812b6d1e236
unknown <viktormoskalev07@gmail.com>
2021-07-14 19:54:47 +0300

    f

--- src/js/slider.js
+++ src/js/slider.js
@@ -70,9 +70,54 @@ var swiper = new Swiper(".section-reviews__swiper", {
     }
   });
 
+
+
+
  
 
 
+  function videoInSlider(){
+
+    const videoSlide = document.querySelector('.hero-video-slide'); 
+    const videoPreloader = document.querySelector('.hero-video-slide__preloader'); 
+    video=document.getElementById('hero-video'); 
+    let checkVideo =1 ;
+  function call (){ 
+    if (checkVideo&&videoSlide){ 
+      if( videoSlide.matches('.swiper-slide-active')){ 
+                   
+                    video.src=video.dataset.src; 
+                    checkVideo=0;
+                    setTimeout(() => {
+                      videoPreloader.style.opacity=0;
+                    }, 300); 
+                    observer.disconnect();
+      } 
+  }  
+  }
+  const observer = new MutationObserver(call)  
+  observer.observe(videoSlide, {
+    childList:false,
+    attributes:true,
+    characterData:false,
+    attributeFilter:['class']
+  })
+     setTimeout(() => {
+       if(checkVideo){
+        video.src=video.dataset.src; 
+        checkVideo=0;
+        setTimeout(() => {
+          videoPreloader.style.opacity=0;
+        }, 300); 
+        observer.disconnect();
+       }
+     }, 2000);
+} 
+
+
+ 
+if (document.querySelector('.section-hero__tab-activators')){
+ 
   var swiperTabsControl = new Swiper(".section-hero__tab-activators", {  
     slidesPerView: 6,
     direction:'vertical',
@@ -91,6 +136,10 @@ var swiperTabs = new Swiper(".section-hero__swiper", {
     thumbs: {
       swiper: swiperTabsControl
     } 
-  });
+  }); 
 
-  @@include('../video-include/video.js')
+    
+  videoInSlider(); 
+}
+
+
