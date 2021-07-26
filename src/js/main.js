a33b20dde9f2fbf584e3f6ab4206a9920b22831d
MatviienkoNataliia <admin@MatviienkoNataliias-Mac-mini.local>
2021-07-26 16:37:21 +0300

    page 2.10

--- src/js/main.js
+++ src/js/main.js
@@ -88,29 +88,6 @@ function promobar (){
  
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("modalLogin");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
 @@include('modules/imgpreloader.js')
 @@include('modules/accordion.js') 
 @@include('modules/popup-mobile.js')
