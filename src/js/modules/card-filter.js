b3d8f0e5dc7445b2c5588f6c8c538b11655903ea
unknown <viktormoskalev07@gmail.com>
2021-07-27 13:52:48 +0300

    filter

--- src/js/modules/card-filter.js
+++ src/js/modules/card-filter.js
@@ -3,17 +3,13 @@
               const radiocontainer = document.querySelector('.photobook__radio__container');
               const card = document.querySelectorAll('.filtered-card-js');
              
-              if(radiocontainer&&card){
-                  
+              if(radiocontainer&&card){ 
                     radiocontainer.addEventListener('change' , function(e){
-                         card.forEach((item)=>{
-                     
+                         card.forEach((item)=>{ 
                              if(!(item.dataset.type==e.target.value)&&!(e.target.value=='All')){
-                                item.classList.add('card-hide');
-                          
+                                item.classList.add('card-hide'); 
                              } else {
-                                item.classList.remove('card-hide')
-                                
+                                item.classList.remove('card-hide') 
                              }
                          }) 
                     }) 
