e167b2a8e1e2da9758c2df65368b901ca39f01a6
MatviienkoNataliia <admin@MacForMike.local>
2021-08-05 19:26:05 +0300

    page 4.1

--- gulpfile.js
+++ gulpfile.js
@@ -99,42 +99,42 @@ var imageminGiflossy = require('imagemin-giflossy');
 
 gulp.task('image:build', async function() {
   return gulp.src(path.src.img)
-      .pipe(cache(imagemin([
-          //png
-          imageminPngquant({
-              speed: 1,
-              quality: [0.95, 1] //lossy settings
-          }),
-          imageminZopfli({
-              more: true
-              // iterations: 50 // very slow but more effective
-          }),
-          //gif
-          // imagemin.gifsicle({
-          //     interlaced: true,
-          //     optimizationLevel: 3
-          // }),
-          //gif very light lossy, use only one of gifsicle or Giflossy
-          imageminGiflossy({
-              optimizationLevel: 3,
-              optimize: 3, //keep-empty: Preserve empty transparent frames
-              lossy: 2
-          }),
-          //svg
-          imagemin.svgo({
-              plugins: [{
-                  removeViewBox: false
-              }]
-          }),
-          //jpg lossless
-          imagemin.jpegtran({
-              progressive: true
-          }),
-          //jpg very light lossy, use vs jpegtran
-          imageminMozjpeg({
-              quality: 90
-          })
-      ])))
+      // .pipe(cache(imagemin([
+      //     //png
+      //     imageminPngquant({
+      //         speed: 1,
+      //         quality: [0.95, 1] //lossy settings
+      //     }),
+      //     imageminZopfli({
+      //         more: true
+      //         // iterations: 50 // very slow but more effective
+      //     }),
+      //     //gif
+      //     // imagemin.gifsicle({
+      //     //     interlaced: true,
+      //     //     optimizationLevel: 3
+      //     // }),
+      //     //gif very light lossy, use only one of gifsicle or Giflossy
+      //     imageminGiflossy({
+      //         optimizationLevel: 3,
+      //         optimize: 3, //keep-empty: Preserve empty transparent frames
+      //         lossy: 2
+      //     }),
+      //     //svg
+      //     imagemin.svgo({
+      //         plugins: [{
+      //             removeViewBox: false
+      //         }]
+      //     }),
+      //     //jpg lossless
+      //     imagemin.jpegtran({
+      //         progressive: true
+      //     }),
+      //     //jpg very light lossy, use vs jpegtran
+      //     imageminMozjpeg({
+      //         quality: 90
+      //     })
+      // ])))
       .pipe(gulp.dest(path.build.img)); //И бросим в build
 });
 
